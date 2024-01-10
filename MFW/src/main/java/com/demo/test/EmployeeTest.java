6da9cf3f9139639dab8b188505fa83001835ec0b
MANISH KUMAR PRAJAPATI <71719780+ManishkPrajapati@users.noreply.github.com>
2024-01-10 13:18:48 +0530

    Add files via upload

--- MFW/src/main/java/com/demo/test/EmployeeTest.java
+++ MFW/src/main/java/com/demo/test/EmployeeTest.java
@@ -1,9 +1,12 @@
 package com.demo.test;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.demo.model.Employee;
 import com.demo.model.MyUser;
@@ -24,16 +27,19 @@ public class EmployeeTest {
 //		s1.save(e3);
 //		s1.save(m1);
 //		s1.save(m2);
+				
+//		Session s2=sf.openSession();
+//		Transaction tr1=s2.beginTransaction();
+//		Employee emp=s2.get(Employee.class,19 );
+//		System.out.println("ber---");
+//		System.out.println(emp.getEname());
+//		System.out.println("after");
+		Query q =s1.createQuery("from Employee");
+		List<Employee> lst = q.list();
+		System.out.println(lst);
 		tr.commit();
 		s1.close();
-		
-		Session s2=sf.openSession();
-		Transaction tr1=s2.beginTransaction();
-		Employee emp=s2.get(Employee.class,19 );
-		System.out.println("ber---");
-		System.out.println(emp.getEname());
-		System.out.println("after");
-		
+
 	}
 
 }
